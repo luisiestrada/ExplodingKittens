21aa54e0129dc036cf9545227713f84000a004f2
maximilianfischer <maximilianfischer310395@gmail.com>
2016-04-07 20:33:03 -0700

    fixed syntax error

--- db/migrate/20160408024941_create_game_stats.rb
+++ db/migrate/20160408024941_create_game_stats.rb
@@ -1,11 +1,11 @@
 class CreateGameStats < ActiveRecord::Migration
   def change
     create_table :game_stats do |t|
-    	t.int :game_id, null: false, index: true
-    	t.int :user_id, null: false, index: true
-    	t.int :cards_played, null: false, default: 0
-    	t.int :cards_combos_played, null: false, default: 0
-    	t.int :players_killed, null: false, default: 0
+    	t.integer :game_id, null: false, index: true
+    	t.integer :user_id, null: false, index: true
+    	t.integer :cards_played, null: false, default: 0
+    	t.integer :cards_combos_played, null: false, default: 0
+    	t.integer :players_killed, null: false, default: 0
 
     	# foreign key on game_id column of game_stats table
     	add_foreign_key :game_stats, :game
