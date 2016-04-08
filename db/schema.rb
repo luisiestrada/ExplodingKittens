e6576db66c27d785fe57563d9b62dc229bad0b5f
leethomas <leet944@gmail.com>
2016-04-07 19:55:36 -0700

    added user stats table

---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160408024750) do
+
+  create_table "user_stats", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "wins",               default: 0, null: false
+    t.integer "losses",             default: 0, null: false
+    t.integer "cards_played",       default: 0, null: false
+    t.integer "card_combos_played", default: 0, null: false
+    t.integer "players_killed",     default: 0, null: false
+  end
+
+  add_index "user_stats", ["user_id"], name: "index_user_stats_on_user_id"
+
+end
