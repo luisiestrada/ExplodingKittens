73621d8265449288173efd91824af2c291bb53c7
maximilianfischer <maximilianfischer310395@gmail.com>
2016-04-07 20:58:19 -0700

    created model for game-stat

--- db/migrate/20160408024941_create_game_stats.rb
+++ db/migrate/20160408024941_create_game_stats.rb
@@ -1,15 +1,15 @@
 class CreateGameStats < ActiveRecord::Migration
   def change
     create_table :game_stats do |t|
-    	t.integer :game_id, null: false, index: true
-    	t.integer :user_id, null: false, index: true
+
+    	# t.references creates integer columns game_id and user_id
+    	# including index and foreign key constraint
+    	t.references :game, index: true, foreign_key: true
+    	t.references :user, index: true, foreign_key: true
+
     	t.integer :cards_played, null: false, default: 0
     	t.integer :cards_combos_played, null: false, default: 0
     	t.integer :players_killed, null: false, default: 0
-
-    	# foreign key on game_id column of game_stats table
-    	add_foreign_key :game_stats, :game
-    	add_foreign_key :game_stats, :user
     end
   end
 end
