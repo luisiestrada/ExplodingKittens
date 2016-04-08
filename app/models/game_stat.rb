c86c64b80ed194434ed291743765363e57f3bceb
maximilianfischer <maximilianfischer310395@gmail.com>
2016-04-07 21:28:25 -0700

    extended game-stat model, fixed game-stats migration
    Conflicts:
    	app/models/game_stat.rb

--- app/models/game_stat.rb
+++ app/models/game_stat.rb
@@ -2,6 +2,8 @@ class GameStat < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :game
 
-	validates :user_id, presence: true
-	validates :game_id, presence: true
+	validates_presence_of :user_id, :game_id
+
+	validates_numericality_of :cards_played, :cards_combos_played, 
+		:players_killed, only_integer: true, greater_than_or_equal_to: 0
 end
