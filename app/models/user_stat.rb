88adeb97db597c92474fa9191a52c28978181e0a
leethomas <leet944@gmail.com>
2016-04-09 17:37:50 -0700

    one stats table to rule them all

--- app/models/user_stat.rb
+++ app/models/user_stat.rb
@@ -1,15 +1,3 @@
-class UserStat < ActiveRecord::Base
+class UserStat < Stat
   belongs_to :user
-
-  validates_presence_of :user_id
-  validates_numericality_of :wins, :losses, :cards_played, :card_combos_played,
-    :players_killed, only_integer: true, greater_than_or_equal_to: 0
-
-  def games_played
-    self.wins + self.losses
-  end
-
-  def win_loss_ratio
-    self.wins / self.losses
-  end
 end
