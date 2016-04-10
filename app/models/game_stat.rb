20e5bf29bf2ee772f08be0e11000d44860643c72
leethomas <leet944@gmail.com>
2016-04-09 19:01:31 -0700

    refactored user & game_stat

--- app/models/game_stat.rb
+++ app/models/game_stat.rb
@@ -1,9 +1,4 @@
 class GameStat < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :game
-
-	validates_presence_of :user_id, :game_id
-
-	validates_numericality_of :cards_played, :cards_combos_played, 
-		:players_killed, only_integer: true, greater_than_or_equal_to: 0
 end
