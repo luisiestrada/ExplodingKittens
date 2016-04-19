4d6e5854a6966d6442fb00f35e0df8b1e9fb715b
leethomas <leet944@gmail.com>
2016-04-18 20:40:07 -0700

    added Rspec & basic users specs

---
+++ spec/models/users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let (:user) { User.create(email: 'woahbam@sdlfdsf.com', password: 'lsdjflksdjfsd') }
+  describe 'after signup' do
+    it 'a random username is generated' do
+      expect(user.username.blank?).to be_falsy
+    end
+
+    it 'a UserStat object is generated' do
+      expect(user.stats).to be_truthy
+    end
+
+    it 'games played should be 0' do
+      expect(user.games_played).to eql(0)
+    end
+
+    it 'win loss ratio should be 0.0' do
+      expect(user.win_loss_ratio).to eql(0.0)
+    end
+  end
+end
