9791428742b1f73ea610190fb52f834c037bf328
estukalov <dazhdbog@yahoo.com>
2016-04-30 16:24:11 -0700

    duplicate messages bug.

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,3 +1,19 @@
+//= require pusher.min
+
+var pusher = new Pusher('78511c9f413a61ee66ee', {
+  encrypted: true
+});
+
+var display_chat_message = function(data) {
+	    	var msg = '<div class="message"><span><span id="person">' + data.username + ':</span>  ' 
+	    		+ data.message + "</span></div>";
+
+	      $( "div#chatbox" ).append(msg);
+	      console.log("appended");
+	      $('div#chatbox').animate({
+  			scrollTop:$('div#chatbox').get(0).scrollHeight}, 'fast');
+}
+
 var input_is_empty = function() {
 	var value = $('input#chatbox').val();
 	return _.isEmpty(value);
