b46c943c397000bb835242a9e1a806cb1c5d998c
leethomas <leet944@gmail.com>
2016-05-05 21:30:36 -0700

    start game and send game data to frontend

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,30 +1,54 @@
 class GamesController < ApplicationController
+  before_filter :set_game_context, except: [:new, :create, :index]
+
   def index
-      @games = Game.all
+    @games = Game.all
   end
-  
+
   def new
   end
-  
+
   def create
     if current_user
-      logger.debug current_user.inspect
-      @game = Game.new()
-      @game.add_user(User.find(current_user.id))
-     
-      @game.save
-      redirect_to @game
+      @game = Game.new
+      @game.add_user(current_user)
+
+      @game.save!
+
+      flash[:notice] = 'New game!'
+      redirect_to @game and return
+    else
+      flash[:alert] = 'You must be logged in to create a game.'
+      redirect_to root_path and return
     end
   end
-  
+
   def show
-      @game = Game.find(params[:id])
   end
-  
+
+  def start
+    respond_to do |format|
+      format.json do
+        if @game.valid_player_count?
+          @game.start_game!
+          render json: @game.as_json
+        else
+          render json: { error: 'You need at least 2 players to start the game.' }
+        end
+      end
+    end
+  end
+
   def update
-    @game=Game.find(params[:id])
-    @game.add_user(User.find(current_user.id))
-    @game.save
+    @game.add_user(current_user)
+    @game.save!
     redirect_to @game
+  end
+
+  private
+
+  def set_game_context
+    @game = Game.find_by_id(params[:id])
+    not_found unless @game
   end
 end
