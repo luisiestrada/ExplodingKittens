2999c10c28be6745ade02df763b9e66887f127ff
lierluis <lierluis1@gmail.com>
2016-05-06 17:10:50 -0700

    re-added settings.yml

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,14 +1,8 @@
 # config/initializers/pusher.rb
 require 'pusher'
 
-#Pusher.app_id = Settings.pusher.app_id
-#Pusher.key = Settings.pusher.key
-#Pusher.secret = Settings.pusher.secret
-#Pusher.logger = Rails.logger
-#Pusher.encrypted = Settings.pusher.encrypted
-
-Pusher.app_id = '200598'
-Pusher.key = '78511c9f413a61ee66ee'
-Pusher.secret = '27eebd95d42a57bf55a4'
+Pusher.app_id = Settings.pusher.app_id
+Pusher.key = Settings.pusher.key
+Pusher.secret = Settings.pusher.secret
 Pusher.logger = Rails.logger
-Pusher.encrypted = true 
+Pusher.encrypted = Settings.pusher.encrypted
