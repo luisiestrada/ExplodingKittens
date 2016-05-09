5624cf84e5fa6eef56a5ee33ad32e1b4f3350616
leethomas <leet944@gmail.com>
2016-05-08 18:48:18 -0700

    add index to draw_pile_ids

--- db/migrate/20160509011632_add_draw_pile_to_games.rb
+++ db/migrate/20160509011632_add_draw_pile_to_games.rb
@@ -1,5 +1,5 @@
 class AddDrawPileToGames < ActiveRecord::Migration
   def change
-    add_column :games, :draw_pile_ids, :string
+    add_column :games, :draw_pile_ids, :string, index: true
   end
 end
