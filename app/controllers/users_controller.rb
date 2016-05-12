48fd2ec136c1eb65c56a2f75c7772745dfc66724
estukalov <dazhdbog@yahoo.com>
2016-05-12 11:19:38 -0700

    pagination and sorting for "players & stats" page (work in progress).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  
   def index
-      @users = User.all
+      @users = User.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])
+  end
+  
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
