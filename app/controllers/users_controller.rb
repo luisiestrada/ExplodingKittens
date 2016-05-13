723b0b22a633c1c14866a42af053951336aa9bfb
estukalov <dazhdbog@yahoo.com>
2016-05-12 20:44:23 -0700

    sorting and pagination for users page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,15 @@ class UsersController < ApplicationController
   helper_method :sort_column, :sort_direction
   
   def index
-      @users = User.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])
+    @users = User.joins(:stats).order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])
   end
   
   def sort_column
-    User.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    if (!params[:sort].nil?)
+      return (User.column_names.include?(params[:sort]) || UserStat.column_names.include?((params[:sort].sub("stats.","")))) ? params[:sort] : "id"
+    else
+      return "id"
+    end
   end
   
   def sort_direction
